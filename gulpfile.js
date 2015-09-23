474fd31d8aacf3adf8474cc92b96fd76e4ecbf46
Stewart <stewartwadehines@gmail.com>
2015-09-23 00:49:54 -0500

    Styling Blocks
    * Styled the global footer
    * Styled the second call to action
    * Styled the bid block
    * Started the Social Proof block - the images are giving me fits.
    * The whole thing needs to be mobile friendly.

--- gulpfile.js
+++ gulpfile.js
@@ -17,27 +17,27 @@ var browserSync = require('browser-sync').create();
 
 // Serve Task
 gulp.task('serve', function() {
-  browserSync.init({
-    server: "./"
-  });
-  gulp.watch("./assets/scss/*.scss", ['sass']);
-  gulp.watch("./*.html").on('change', browserSync.reload);
+    browserSync.init({
+        server: "./"
+    });
+    gulp.watch("./assets/scss/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
 // 3. Sass Task
 gulp.task('sass', function() {
-  // Feed the source
-  return gulp.src('./assets/scss/*.scss')
-    // What do do with the files?
-    .pipe(sass())
-    // Log the errors
-    .pipe(sass().on('error', sass.logError))
-    // Autoprefix the stuff
-    .pipe(autoprefixer('last 2 versions'))
-    // Where is the file going to go?
-    .pipe(gulp.dest('./assets/css'))
-    // Browser Sync
-    .pipe(browserSync.stream());
+    // Feed the source
+    return gulp.src('./assets/scss/*.scss')
+        // What do do with the files?
+        .pipe(sass())
+        // Log the errors
+        .pipe(sass().on('error', sass.logError))
+        // Autoprefix the stuff
+        .pipe(autoprefixer('last 4 versions'))
+        // Where is the file going to go?
+        .pipe(gulp.dest('./assets/css'))
+        // Browser Sync
+        .pipe(browserSync.stream());
 });
 
 
